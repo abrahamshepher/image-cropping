55ade1981ce98974a0c73438413b85bff43cdf31
abrahamshepher <abrahamshepher@gmail.com>
2025-02-19 16:18:40 +0500

    looks good

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  images: {
+    domains: ["localhost"],
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        crypto: false,
+      };
+    }
+    return config;
+  },
+  experimental: {
+    serverComponentsExternalPackages: ["@aws-sdk"],
+  },
 };
 
 export default nextConfig;

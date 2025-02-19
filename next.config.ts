61e044431e88d0d4d983d49762a6d2719970b163
abrahamshepher <abrahamshepher@gmail.com>
2025-02-19 16:46:57 +0500

    fixes

--- next.config.ts
+++ next.config.ts
@@ -4,19 +4,6 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["localhost"],
   },
-  webpack: (config, { isServer }) => {
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        fs: false,
-        crypto: false,
-      };
-    }
-    return config;
-  },
-  experimental: {
-    serverComponentsExternalPackages: ["@aws-sdk"],
-  },
 };
 
 export default nextConfig;

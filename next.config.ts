46cc9a90755ec501767b5e0558d9602789bced40
abrahamshepher <abrahamshepher@gmail.com>
2025-02-20 13:08:26 +0500

    seems fixed

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,19 @@ const nextConfig: NextConfig = {
   images: {
     domains: ["localhost"],
   },
+  webpack: (config) => {
+    config.resolve.fallback = { fs: false };
+    return config;
+  },
+  rules: {
+    "react-hooks/exhaustive-deps": "off",
+    "react/no-unescaped-entities": "off",
+  },
+  reactStrictMode: true,
+
 };
+const withTM = require('next-transpile-modules')(['face-api.js']);
+
+
 
 export default nextConfig;
